698d688f741933f22a4ad675b825677f876d7b4c
samvk <samvnkauffman@gmail.com>
2016-10-14 23:54:33 -0400

    latest

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path'),
+      SRC = path.join(__dirname, 'src/'),
+      NODE_MODULES = path.join(__dirname, 'node_modules/');
+
+const config = {
+    entry: "./src",               // entry point
+    output: {                     // output folder
+        path: "./dist",           // folder path
+        filename: "app.js"        // file name
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ["babel"]   // babelify
+            }
+        ]
+    },
+    resolve: {
+        root: [SRC, NODE_MODULES]   // root folders for Webpack resolving
+    }
+}
+
+module.exports = config;
