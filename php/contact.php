c8d1be6ff6900028309cada1fed5e3153ecc1936
samvk <samvnkauffman@gmail.com>
2016-10-17 07:13:32 -0400

    working

--- php/contact.php
+++ php/contact.php
@@ -6,18 +6,14 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
 	exit;
 }
 
-require "libraries/mail/class.phpmailer.php";
-
-//data
 $to = trim($_POST["to"]);
 $from = trim($_POST["from"]);
 $body = trim($_POST["body"]);
-$user_email = trim($_POST["user-email"]);
 $target_email = trim($_POST["target-email"]);
 $image = trim($_POST["image"]);
 
 //Empty fields check (only if bypassed client-side validation)
-if ( empty($to) || empty($from) || empty($user_email) || empty($target_email) ) {
+if ( empty($to) || empty($from) || empty($target_email) ) {
 	$response = array(
 		"status" => "error",
 		"message" => "You left some required fields blank."
@@ -26,17 +22,8 @@ if ( empty($to) || empty($from) || empty($user_email) || empty($target_email) ) 
 	exit;
 }
 
-//set mail info
-$mail = new PHPMailer(true);
-
-$mail->AddAddress($target_email);
-$mail->SetFrom("noreply@thegreetingsworld.com");
-
-$mail->Subject = "You've Recieved a Special Greeting from {$from} | Greetings, World!";
-
-$mail->IsHTML(true);
-
-$mail->AddEmbeddedImage($image, "imageSrc");
+// Send message to email address
+$subject = "You've Recieved a Special Greeting from {$from} | Greetings, World!";
 
 $message = "
 <html>
@@ -45,15 +32,16 @@ $message = "
 	</head>
 	<body>
 		<p>{$body}</p>
-		<img src='cid:imageSrc' alt='Your greeting card'>
+		<img src='{$image}' alt='Your greeting card'>
 	</body>
 </html>
 ";
 
-$mail->Body = $message;
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From: noreply@thegreetingsworld.com\n";
 
-//send mail
-$mail->Send();
+mail($target_email, $subject, $message, $headers);
 
 $response = array(
 	"status" => "success",
@@ -61,13 +49,3 @@ $response = array(
 );
 
 echo json_encode($response);
-
-/*
-$imageFile = base64_decode($image);
-$imageFile2 = str_replace(' ', '+', $imageFile);
-$image2 = str_replace(' ', '+', $image);
-*/
-//no//$mail->AddStringAttachment($imageFile, "greetings.png", "base64", "image/png");
-//no//$mail->AddStringAttachment($imageFile2, "greetings.png", "base64", "image/png");
-//no//$mail->AddStringAttachment($image, "greetings.png", "base64", "image/png");
-//no//$mail->AddStringAttachment($image2, "greetings.png", "base64", "image/png");
